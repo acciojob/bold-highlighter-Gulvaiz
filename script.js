1233cee09951a524d7e7efc1309d2f2f1f8798b5
Gulvaiz Alam <gulvaizalam12@gmail.com>
2024-08-05 13:16:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+let strongEle = document.querySelectorAll("strong")
 function highlight() {
     //Write your code here
-
+ strongEle.forEach((strong) => {
+	  strong.style.color = "green"
+ })
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+ strongEle.forEach((strong) => {
+	  strong.style.color = "black"
+ })
     
 }
